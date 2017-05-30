92c630cb563e6cdf18e460ef7a63ad06da03044f
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-05-29 20:46:53 -0700

    merged data!

--- code.py
+++ code.py
@@ -1,9 +1,19 @@
 import pandas as pd
+import functools as functools
+
+# import, combine datax
 dataGen = pd.read_stata("data/general.dta")
 dataHealth = pd.read_stata("data/health and services.dta")
 
-# list column names
+## common column names
 list(dataGen.columns.values)
+set(dataGen.columns.values).intersection( # common values: ano, codmpio
+    set(dataHealth.columns.values))
+
+dataAll = functools.reduce(
+    lambda left,right: pd.merge(left,right,how='inner'
+             ,on=['ano','codmpio'])
+    , [dataGen,dataHealth]) # can add datasets here
 
 # list unique values in a column
 dataGen.gandina.unique()
@@ -13,5 +23,4 @@ a = np.array([[1,2,3], [4,5,np.nan], [7,8,9]]);
 a[~np.isnan(a).any(axis=1)]
   # ~ is the not operator
   # axis=1 means "rows"
-
 
