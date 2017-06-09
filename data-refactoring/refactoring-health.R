b7876659f91f5148a2896495b8dffc45d75698a6
Larry-Hignight <larry.hignight@gmail.com>
2017-06-09 01:17:32 -0700

    Added file writing and removed the analysis from refacting-health.R;  Created a new health.csv file.

--- data-refactoring/refactoring-health.R
+++ data-refactoring/refactoring-health.R
@@ -11,18 +11,11 @@ library(lubridate)
 health$year <- year(ymd(health$year))
 table(health$year)
 
-# What percentage of data is found in each column of the file?
-col_fraction_missing <- 100 * apply(health, 2, function(col) sum(is.na(col)) / nrow(health))
-plot(1:ncol(health), col_fraction_missing, type = 'l', col = 'blue',
-     main = 'Health - Missing Data', xlab = 'Col #', ylab = 'Percent Missing')
-abline(h = 50, col = 'red')
+## Write the refactored data to the refactored-data directory
+write.csv(health, file = 'refactored-data/health.csv', row.names = FALSE)
+x <- read.csv('refactored-data/health.csv', header = T)
 
-#fraction_order <- order(col_fraction_missing)
-#cbind(colnames(health)[fraction_order], col_fraction_missing[fraction_order])
-
-
-# Remove columns where > 50% of the data is NA
-#cols.rm <- apply(health, 2, function(col) sum(is.na(col)) / nrow(health) >= .5)
-#colnames(health)[cols.rm]  # These cols are going to be removed
-#colnames(health)[!cols.rm] # These cols are going to be kept
-#health <- health[ , !cols.rm]
+# Compare the values in the old and new files
+x <- read.csv('refactored-data/health.csv', header = T)
+diff <- sapply(1:ncol(x), function(n) max(abs(health[ , n] - x[ , n]), na.rm = TRUE))
+max(diff)
