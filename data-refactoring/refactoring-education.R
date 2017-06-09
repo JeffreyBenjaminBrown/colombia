60e775c7a03820ad5837d259b86428fc54539243
Larry-Hignight <larry.hignight@gmail.com>
2017-06-08 19:26:57 -0700

    Changed x to general and education in the data refactoring files.

--- data-refactoring/refactoring-education.R
+++ data-refactoring/refactoring-education.R
@@ -1,28 +1,29 @@
 # Refactoring the education.csv file from the Colombia data
 
-x <- read.csv('csv/education.csv', header = TRUE)
-x <- x[ , -1]   # Remove the index column
+education <- read.csv('csv/education.csv', header = TRUE)
+education <- education[ , -1]   # Remove the index column
 
 # Working on the first seven columns; rename them into English
-colnames(x)[1:2] <- c('municipality_code', 'year')
+colnames(education)[1:2] <- c('municipality_code', 'year')
 
 # The year to a single year
 library(lubridate)
-x$year <- year(ymd(x$year))
-table(x$year)
+education$year <- year(ymd(education$year))
+table(education$year)
 
 # What percentage of data is found in each column of the file?
-col_fraction_missing <- 100 * apply(x, 2, function(col) sum(is.na(col)) / nrow(x))
-plot(1:ncol(x), col_fraction_missing, type = 'l', col = 'blue', 
+col_fraction_missing <- 100 * apply(education, 2, function(col) sum(is.na(col)) / nrow(education))
+plot(1:ncol(education), col_fraction_missing, type = 'l', col = 'blue', 
      main = 'Missing Data Percentage', xlab = 'Col #', ylab = 'Percentage')
+abline(h = 50, col = 'red')
 
 fraction_order <- order(col_fraction_missing)
-cbind(colnames(x)[fraction_order], col_fraction_missing[fraction_order])
+cbind(colnames(education)[fraction_order], col_fraction_missing[fraction_order])
 
 
 # Remove columns where > 50% of the data is NA
-#cols.rm <- apply(x, 2, function(col) sum(is.na(col)) / nrow(x) >= .5)
-#colnames(x)[cols.rm]  # These cols are going to be removed
-#colnames(x)[!cols.rm] # These cols are going to be kept
-#x <- x[ , !cols.rm]
+#cols.rm <- apply(education, 2, function(col) sum(is.na(col)) / nrow(education) >= .5)
+#colnames(education)[cols.rm]  # These cols are going to be removed
+#colnames(education)[!cols.rm] # These cols are going to be kept
+#education <- education[ , !cols.rm]
 
--- data-refactoring/refactoring-general.R
+++ data-refactoring/refactoring-general.R
@@ -1,19 +1,18 @@
 # Refactoring the general.csv file from the Colombia data
 
-x <- read.csv('csv/general.csv', header = TRUE)
-                #colClasses = c(rep('integer', 3), rep('factor', 3), 'double'))
-x <- x[ , -1]   # Remove the index column
+general <- read.csv('csv/general.csv', header = TRUE)  #colClasses = c(rep('integer', 3), rep('factor', 3), 'double'))
+general <- general[ , -1]   # Remove the index column
 
 # Working on the first seven columns; rename them into English
-colnames(x)[1:7] <- c('dept_code', 'province_code', 'municipality_code', 'dept', 'province', 'municipality', 'year')
+colnames(general)[1:7] <- c('dept_code', 'province_code', 'municipality_code', 'dept', 'province', 'municipality', 'year')
 
 # Changing the first 3 columns to integer
-x$dept_code <- as.integer(x$dept_code)
-x$province_code <- as.integer(x$province_code)
-x$municipality_code <- as.integer(x$municipality_code)
+general$dept_code <- as.integer(general$dept_code)
+general$province_code <- as.integer(general$province_code)
+general$municipality_code <- as.integer(general$municipality_code)
 
 # TODO - The mapping between codes and names isn't 1 to 1
-sapply(colnames(x)[1:6], function(col) length(unique(x[ , col])))
+sapply(colnames(general)[1:6], function(col) length(unique(general[ , col])))
 
 # TODO - Merge the 5 regions columns into a single region with the value
 regions <- c('gandina', 'gcaribe', 'gpacifica', 'gorinoquia', 'gamazonia')
@@ -22,48 +21,48 @@ regions <- c('gandina', 'gcaribe', 'gpacifica', 'gorinoquia', 'gamazonia')
 # Change the Stata time encoding to a year
 library(lubridate)
 convert_timestamp <- function(x) year(as.POSIXct(x/1000000000, origin = '1970-01-01')) + 1
-x$year <- sapply(x$year, convert_timestamp)
+general$year <- sapply(general$year, convert_timestamp)
 
 # Replace the "" in municipality with NA values
   # >> todo: make into a togglable assumption
-x$municipality[x$municipality == ""] <- NA
+general$municipality[general$municipality == ""] <- NA
 
 # Row 23 is missing is the muni_code == 5001 with missing dept and prov data
-tail(x[x$municipality_code == 5001, 1:7])
-x$dept_code[23] <- 5
-x$province_code[23] <- 595
-x$dept[23] <- 'Antioquia'
-x$province[23] <- 'Valle del aburra'
-x$municipality <- 'Medellín'
-tail(x[x$municipality_code == 5001, 1:7])
+tail(general[general$municipality_code == 5001, 1:7])
+general$dept_code[23] <- 5
+general$province_code[23] <- 595
+general$dept[23] <- 'Antioquia'
+general$province[23] <- 'Valle del aburra'
+general$municipality <- 'Medellín'
+tail(general[general$municipality_code == 5001, 1:7])
 
 # Do the dept and province have NA for the same rows?  YES!
 # there is no explicit set data type in base R
 # 2015 is the only year for which this data is missing
-setdiff(which(is.na(x$dept_code)), which(is.na(x$province_code)))
-setdiff(which(is.na(x$dept)), which(is.na(x$province_code)))
-setdiff(which(is.na(x$dept)), which(is.na(x$province)))
-table(x$year[is.na(x$dept)])
+setdiff(which(is.na(general$dept_code)), which(is.na(general$province_code)))
+setdiff(which(is.na(general$dept)), which(is.na(general$province_code)))
+setdiff(which(is.na(general$dept)), which(is.na(general$province)))
+table(general$year[is.na(general$dept)])
 
 # What percentage of data is found in each column of the file?
-col_fraction_missing <- apply(x, 2, function(col) sum(is.na(col)) / nrow(x))
-plot(1:ncol(x), col_fraction_missing
+col_fraction_missing <- apply(general, 2, function(col) sum(is.na(col)) / nrow(general))
+plot(1:ncol(general), col_fraction_missing
     , type = 'l', col = 'blue', xlab = 'Col #', ylab = 'Percentage')
 
 fraction_order <- order(col_fraction_missing)
-cbind(colnames(x)[fraction_order], col_fraction_missing[fraction_order])
+cbind(colnames(general)[fraction_order], col_fraction_missing[fraction_order])
 # todo: the last many columns are 95%-ish missing
 # was that a single extremely-informative year?
 
 # Remove columns where > 50% of the data is NA
-cols.rm <- apply(x, 2, function(col) sum(is.na(col)) / nrow(x) >= .5)
-colnames(x)[cols.rm]  # These cols are going to be removed
-colnames(x)[!cols.rm] # These cols are going to be kept
-x <- x[ , !cols.rm]
+#cols.rm <- apply(general, 2, function(col) sum(is.na(col)) / nrow(general) >= .5)
+#colnames(general)[cols.rm]  # These cols are going to be removed
+#colnames(general)[!cols.rm] # These cols are going to be kept
+#general <- general[ , !cols.rm]
 
 
 ## Jeff - We're going to need translations for these names!
-#> colnames(x)
+#> colnames(general)
 # [1] "dept_code"         "province_code"     "municipality_code"
 # [4] "dept"              "province"          "municipality"     
 # [7] "year"              "ao_crea"           "act_adm"          
@@ -76,8 +75,7 @@ x <- x[ , !cols.rm]
 #[28] "gpc"               "gini"             
 
 
-
 # Plot the cors to find interesting ones
-tbl <- cor(x[ , 10:ncol(x)], use = 'complete.obs')
-for (i in 1:nrow(tbl)) tbl[i, i] <- 0    # Set the diagonal to 0
-for (i in 1:nrow(tbl)) plot(1:nrow(tbl), tbl[ , i], type = 'l', col = 'blue', main = rownames(tbl)[i])
+#tbl <- cor(general[ , 10:ncol(general)], use = 'complete.obs')
+#for (i in 1:nrow(tbl)) tbl[i, i] <- 0    # Set the diagonal to 0
+#for (i in 1:nrow(tbl)) plot(1:nrow(tbl), tbl[ , i], type = 'l', col = 'blue', main = rownames(tbl)[i])
