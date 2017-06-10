49d5356fa752a4ac3e3952007fd0d70d7f0a39a8
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-06-09 23:54:21 -0700

    changes to a file we're not really using

--- not-really-using/code.py
+++ not-really-using/code.py
@@ -1,26 +1,15 @@
-import pandas as pd
-import functools as functools
+# boxplots: gini by year
+varsWithNans=dataAll[['ano','codmpio','gini']];
+vars = varsWithNans[ ~dataAll['gini'].isnull() ]
 
-# import, combine datax
-dataGen = pd.read_stata("data/general.dta")
-dataHealth = pd.read_stata("data/health and services.dta")
+b_vals = vars.ano.unique()
+slices = []
+for i in b_vals: slices.append(vars[vars['ano']==i])
 
-## common column names
-list(dataGen.columns.values)
-set(dataGen.columns.values).intersection( # common values: ano, codmpio
-    set(dataHealth.columns.values))
-
-dataAll = functools.reduce(
-    lambda left,right: pd.merge(left,right,how='inner'
-             ,on=['ano','codmpio'])
-    , [dataGen,dataHealth]) # can add datasets here
-
-# list unique values in a column
-dataGen.gandina.unique()
-
-# strip rows with NaN values
-a = np.array([[1,2,3], [4,5,np.nan], [7,8,9]]);
-a[~np.isnan(a).any(axis=1)]
-  # ~ is the not operator
-  # axis=1 means "rows"
-
+plt.figure()
+plt.boxplot(
+    #slices[1]['gini']
+    [ s['gini'] for s in slices ]
+    ,labels=b_vals
+)
+plt.show()
