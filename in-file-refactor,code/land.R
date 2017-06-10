d173267e55a64c96e6afc0e3ccea798ad74f3120
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-06-09 19:40:45 -0700

    original spanish column names

--- in-file-refactor,code/land.R
+++ in-file-refactor,code/land.R
@@ -3,14 +3,11 @@
 land <- read.csv('csv/land.csv', header = TRUE)
 land <- land[ , -1]   # Remove the index column
 
-# Working on the first seven columns; rename them into English
-colnames(land)[1:2] <- c('municipality_code', 'year')
-
 # Convert the ano column data to a year
 library(lubridate)
 convert_timestamp <- function(x) year(as.POSIXct(x/1000000000, origin = '1970-01-01')) + 1
-land$year <- sapply(land$year, convert_timestamp)
-table(land$year)
+land$ano <- sapply(land$ano, convert_timestamp)
+table(land$ano)
 
 ## Write the refactored data to the refactored-data directory
 write.csv(land, file = 'refactored-data/land.csv', row.names = FALSE)
