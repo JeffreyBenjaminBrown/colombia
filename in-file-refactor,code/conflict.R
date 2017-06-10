28c1123e04ce575e3f248b8b73419e278c464510
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-06-09 18:06:48 -0700

    same thing

--- in-file-refactor,code/conflict.R
+++ in-file-refactor,code/conflict.R
@@ -3,18 +3,15 @@
 conflict <- read.csv('csv/conflict.csv', header = TRUE)
 conflict <- conflict[ , -1]   # Remove the index column
 
-# Working on the first seven columns; rename them into English
-colnames(conflict)[1:2] <- c('municipality_code', 'year')
-
 # Convert the ano column data to a year
 library(lubridate)
 convert_timestamp <- function(x) year(as.POSIXct(x/1000000000, origin = '1970-01-01')) + 1
-conflict$year <- sapply(conflict$year, convert_timestamp)
-table(conflict$year)
+#conflict$ano
+conflict$ano <- sapply(conflict$ano, convert_timestamp)
+table(conflict$ano)
 
 ## Write the refactored data to the refactored-data directory
 write.csv(conflict, file = 'refactored-data/conflict.csv', row.names = FALSE)
-
 
 # Compare the values in the old and new files
 x <- read.csv('refactored-data/conflict.csv', header = T)
