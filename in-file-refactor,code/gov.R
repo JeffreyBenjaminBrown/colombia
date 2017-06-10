91aaea83b87217da30292276710bef157af66f22
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-06-09 19:37:01 -0700

    original Spanish

--- in-file-refactor,code/gov.R
+++ in-file-refactor,code/gov.R
@@ -3,17 +3,15 @@
 gov <- read.csv('csv/gov.csv', header = TRUE)
 gov <- gov[ , -1]   # Remove the index column
 
-# Working on the first seven columns; rename them into English
-colnames(gov)[1:2] <- c('municipality_code', 'year')
-
 # Convert year to an integer
-gov$year <- as.integer(gov$year)
+gov$ano <- as.integer(gov$ano)
+table(gov$ano)
 
 ## Write the refactored data to the refactored-data directory
-write.csv(gov, file = 'refactored-data/gov.csv', row.names = FALSE)
+write.csv(gov, file = 'data,in-file-refactored/gov.csv', row.names = FALSE)
 
 
 # Compare the values in the old and new files
-x <- read.csv('refactored-data/gov.csv', header = T)
+x <- read.csv('data,in-file-refactored/gov.csv', header = T)
 diff <- sapply(1:ncol(x), function(n) max(abs(gov[ , n] - x[ , n]), na.rm = TRUE))
 max(diff)
