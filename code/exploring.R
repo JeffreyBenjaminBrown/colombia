5196eb27312b38b48c8986c472a2d9107bc3cc43
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-06-13 23:20:59 -0700

    order exploratory code just enough to share it

--- code/exploring.R
+++ code/exploring.R
@@ -1,5 +1,8 @@
+# ==== ==== Load & refactor data
 setwd('colombia')
 colombia <- read.csv('data/merged.csv')
+
+colombia$pobl_log <- log( colombia$pobl_tot ) / log(10)
 
 colombia$nacimientos_per_kp <- 1000 * colombia$nacimientos / colombia$pobl_tot
 summary( colombia[c('nacimientos_per_kp','nacimientos','pobl_tot') ] )
@@ -10,13 +13,9 @@ summary( colombia[c('pib_pb_derived','pib_total', 'pobl_tot')])
 colombia$prestadores_pk_derived <- 1000 * colombia$prestadores / colombia$pobl_tot
 summary( colombia[c('prestadores_pk_derived','prestadores', 'pobl_tot')])
 
-colombia$pobl_log <- log( colombia$pobl_tot ) / log(10)
 
-# handy is a more easily human-grokkable subset of colombia
-handy <-  colombia[c('ano', 'municipio', 'pobl_tot', 'pobl_log', 'nacimientos_per_kp', 'prestadores', 'prestadores_pk_derived', 'pib_percapita', 'ipm_hacinam_p', 'ipm_templeof_p', 'gpc', 'gini', 'ipm_analf_p', 'minorias', 'parques', 'religioso', 'estado', 'otras', 'pib_precapita_cons', 'pib_agricola', 'pib_industria', 'pib_servicios', 'pib_total', 'indrural', 'discapital', 'dismdo', 'disbogota')]
-
-
-# Pictures
+# ==== ==== Pictures
+# log of population X births per thousand persons
 cols_to_show <- c('pobl_log','nacimientos_per_kp')
 x <- colombia[ c( 'ano', cols_to_show ) ] # c() always produces a flat list
 x <- na.omit(x)
@@ -24,8 +23,11 @@ x <- x[  sapply( x[ cols_to_show[1] ], is.finite  )
        & sapply( x[ cols_to_show[2] ], is.finite  ), ]
   # ? why must I use sapply( x[cols_to_show[2]], is.finite )
     # instead of is.finite( x[ cols_to_show[2] ]  )
+    # (Someone online said you could check if it had|was that kind of method.)
 plot( x[ x['ano']==2010, cols_to_show] )
 
+
+# log of population X healthcare providers per thousand persons
 cols_to_show <- c('pobl_log','prestadores_pk_derived')
 x <- colombia[ c( 'ano', cols_to_show ) ]
 x <- na.omit(x)
@@ -36,38 +38,47 @@ plot( x[ x['ano']==2010, cols_to_show] )
   # they are easily distinguished visually, but no math yet describes that
   # see figure, "hospitals per k capita X population.png"
 
-# GPC: mostly zeroes?
+
+# log of population X government spending per person
 cols_to_show <- c('pobl_log','gpc')
 x <- colombia[ c( 'ano', cols_to_show ) ]
 x <- na.omit(x)
 x <- x[  sapply( x[ cols_to_show[1] ], is.finite  )
        & sapply( x[ cols_to_show[2] ], is.finite  ), ]
 plot( x[ x['ano']==2010, cols_to_show] )
+  # ? GPC: mostly zeroes
 
 
-ano
-mpio
-pobl_tot
-pobl_log
-prestadores
-ipm_hacinam_p
-gpc
-ipm_analf_p
-parques
-estado
-pib_precapita_cons
-pib_industria
-pib_total
-discapital
-disbogota
-nacimientos_per_kp
-pib_percapita
-ipm_templeof_p
-gini
-minorias
-religioso
-otras
-pib_agricola
-pib_servicios
-indrural
-dismdo
+# log of population X illiteracy in 1985
+var1 <- 'pobl_log'
+var2 <- 'per_alfa1985'
+x <- colombia[ c( 'ano', var1, var2 ) ]
+x <- na.omit(x)
+x <- x[  sapply( x[ var1 ], is.finite  )
+       & sapply( x[ var2 ], is.finite  ), ]
+plot( x[ x['ano']==2010, c(var1,var2)] )
+
+
+# log of population X overcrowding
+var1 <- 'pobl_log'
+var2 <- 'ipm_hacinam_p'
+x <- colombia[ c( 'ano', var1, var2 ) ]
+x <- na.omit(x)
+x <- x[  sapply( x[ var1 ], is.finite  )
+       & sapply( x[ var2 ], is.finite  ), ]
+plot( x[ x['ano']==2010, c(var1,var2)] )
+# ? Why does this draw no picture?
+  # I get "Error in plot.window(...) : need finite 'xlim' values".
+summary( x[ x['ano']==2010] )
+# !? Why is this giving me year 2005?
+
+
+# ==== ==== a function for those, doesn't work
+plotColombia2010 <- function (colName1,colName2) {
+    x <- colombia[c('ano',colName1,colName2)]
+    x <- na.omit(x)
+    x <- x[  sapply( x[ colName1 ], is.finite  )
+           & sapply( x[ colName2 ], is.finite  ), ]
+    plot( x[ x['ano']==2010, colName1, colName2] )
+    }
+
---
+++ documentation/few-vars,in-English.md
@@ -0,0 +1,29 @@
+# -- some variables, in English
+ano                           # year
+municipio                     # city
+pobl_tot                      # total population
+pobl_log                      # log of population
+prestadores                   # health care providers
+prestadores_pk_derived        # health care providers per 1000 people, derived
+ipm_hacinam_p                 # crowding (one of the IPM poverty variables)
+gpc                           # spending per person
+ipm_analf_p                   # illiteracy (IPM=poverty)
+parques                       # parks (area)
+estado                        # state-owned (area)
+minorias                      # ethnic minorities, area
+religioso                     # religious, area
+otras                         # other, area
+pib_total                     # gdp, total
+pib_percapita                 # gdp per person
+pib_precapita_cons            # gdp per person, in constant 2005 terms
+pib_industria                 # gdp, industrial
+pib_agricola                  # gdp, agriculture
+pib_servicios                 # gdp, services
+discapital                    # distance to (province?) capital
+disbogota                     # distance to bogota
+nacimientos_per_kp            # births per 1000 people, derived
+ipm_templeof_p                # employment -- poverty of?
+gini                          # Gini coefficient (inequality)
+indrural                      # index of rurality (if that's a word)
+dismdo                        # distance to the primary market
+
